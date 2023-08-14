56b1708756a3a4a00afb8027f9968cd0dc43b553
Ciaran Cairns <ciarancairns@googlemail.com>
2023-08-14 10:27:12 +0100

    sorta fixed animations but broke scrolling background

--- src/character_sprites/char_class.py
+++ src/character_sprites/char_class.py
@@ -1,15 +1,22 @@
 import pygame
+import sys
 from config.config_manager import ConfigService
 
 config_service = ConfigService()
 config = config_service.get_config()
 
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.animation_speed = 70
-        self.animation_counter = 0
+        self.animation_speed = 60
+        self.speed = 5  # Increased movement speed for smoother gameplay
+        self.jump_speed = -12
+        self.gravity = 1
+        self.is_jumping = False
+        self.old_x = x
+        self.current_action = 'ready'
+        self.velocity = [0, 0]  # [x_velocity, y_velocity]
+
         self.animation_frames = {
             'ready': [],
             'walk': [],
@@ -26,61 +33,79 @@ class Player(pygame.sprite.Sprite):
                 frame = pygame.transform.scale(frame, (config['player_width'], config['player_height']))
                 self.animation_frames[action].append(frame)
 
-        self.current_action = 'ready'
         self.frame_index = 0
         self.image = self.animation_frames[self.current_action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 0.49  # Increased movement speed for smoother gameplay
-        self.jump_speed = -12
-        self.gravity = 1
-        self.is_jumping = False
-        self.old_x = x
+
+    def attack(self):
+        if self.current_action != 'attack1':
+            self.current_action = 'attack1'
 
     def update_animation(self, current_time):
         time_elapsed = current_time - self.last_update_time
-
         if time_elapsed >= self.animation_speed:
             self.frame_index = (self.frame_index + 1) % len(self.animation_frames[self.current_action])
             self.image = self.animation_frames[self.current_action][self.frame_index]
             self.last_update_time = current_time
 
+            if self.current_action == 'attack1' and self.frame_index == len(self.animation_frames['attack1']) - 1:
+                self.current_action = 'ready'
+
+            if self.current_action == 'jump' and self.frame_index == len(self.animation_frames['jump']) - 1:
+                self.current_action = 'ready'
+
+            self.rect.x += self.velocity[0]
+            self.update_jump()
+
     def move(self, direction):
         if direction == 'right':
+            self.velocity[0] = self.speed
             self.current_action = 'walk'
-            self.rect.x += self.speed
-            self.old_x = self.rect.x
         elif direction == 'left':
+            self.velocity[0] = -self.speed
             self.current_action = 'walk'
-            self.rect.x -= self.speed
-            self.old_x = self.rect.x
+        else:
+            self.velocity[0] = 0
 
     def jump(self):
         if not self.is_jumping:
             self.is_jumping = True
+            self.velocity[1] = self.jump_speed
             self.current_action = 'jump'
-            self.jump_speed = -12
 
     def update_jump(self):
         if self.is_jumping:
-            self.rect.y += self.jump_speed
-            self.jump_speed += self.gravity
+            self.rect.y += self.velocity[1]
+            self.velocity[1] += self.gravity
             if self.rect.y >= config['ground_level']:
                 self.is_jumping = False
                 self.rect.y = config['ground_level']
-
-    def attack(self):
-        if self.current_action != 'attack1':
-            self.current_action = 'attack1'
-            self.frame_index = 0  # Reset frame index for attack animation
-
-    def is_moving(self):
-        if self.rect.x != self.old_x:
-            return True
-        else:
-            return False
+                self.velocity[1] = 0
 
     def stop(self):
-        self.current_action = 'ready'
-        self.frame_index = 0  # Reset frame index when stopping
+        if self.current_action == 'walk':
+            self.current_action = 'ready'
+            self.velocity[0] = 0  # Set horizontal velocity to 0 when stopping
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_f:
+                    self.attack()
+                elif event.key == pygame.K_LEFT:
+                    self.move('left')
+                elif event.key == pygame.K_RIGHT:
+                    self.move('right')
+                elif event.key == pygame.K_SPACE:
+                    self.jump()
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    self.stop()
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
--- src/levels/level_1.py
+++ src/levels/level_1.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 from config.config_manager import ConfigService
 from character_sprites.char_class import Player
 
@@ -21,30 +20,8 @@ class Level1Screen:
         self.BG = pygame.transform.scale(self.BG, (config['screen_width'], config['screen_height']))
         self.bg_x = 0
         self.bg_y = 0
+        self.current_time = 0
         self.player = Player(100, 450)
-
-    def handle_input(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_RIGHT]:
-            self.player.move('right')
-            self.bg_scroll += self.bg_speed
-        elif keys[pygame.K_LEFT]:
-            self.player.move('left')
-            self.bg_scroll -= self.bg_speed
-        elif keys[pygame.K_f]:
-            if self.player.current_action != 'attack1':
-                self.player.attack()
-        elif keys[pygame.K_SPACE]:
-            self.player.jump()
-        else:
-            self.player.stop()
-
-    def handle_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
 
     def update_display(self):
         current_time = pygame.time.get_ticks()
@@ -55,11 +32,6 @@ class Level1Screen:
             self.SCREEN.blit(self.BG, (i * self.bg_width - self.bg_scroll, self.bg_y))
         self.SCREEN.blit(self.player.image, self.player.rect)
         self.player.update_animation(current_time)
-        pygame.display.update()
-
-        self.bg_x -= self.bg_speed
-        if self.bg_x <= -self.bg_width:
-            self.bg_x = 0
 
         pygame.display.update()
 
@@ -67,8 +39,10 @@ class Level1Screen:
         clock = pygame.time.Clock()
 
         while True:
-            self.handle_input()
-            self.handle_events()
+            self.player.handle_events()
+            # Update animations and display
+            current_time = pygame.time.get_ticks()
+            self.player.update_animation(current_time)
             self.update_display()
 
             pygame.mixer.Sound.play(self.sound)
